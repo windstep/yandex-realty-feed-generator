76646fc441d7a7188f2ac369367c76ec9626fb59
windstep <51027899+windstep@users.noreply.github.com>
2019-06-21 09:20:58 +0300

    Added more house types

--- src/Enums/BuildingType.php
+++ src/Enums/BuildingType.php
@@ -10,10 +10,16 @@ use MyCLabs\Enum\Enum;
  * @method static BRICK()
  * @method static MONOLITH()
  * @method static PANEL()
+ * @method static MONOLITH_BRICK()
+ * @method static WOOD()
+ * @method static BLOCK()
  */
 class BuildingType extends Enum
 {
     private const BRICK = 'кирпичный';
     private const MONOLITH = 'монолит';
     private const PANEL = 'панельный';
+    private const MONOLITH_BRICK = 'кирпично-монолитный';
+    private const WOOD = 'деревянный';
+    private const BLOCK = 'блочный';
 }
