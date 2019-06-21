f12c42fb16526d9049401e3be60beae85409fa6e
SVV-team <48009651+SVV-team@users.noreply.github.com>
2019-06-21 15:35:14 +0300

    Add new type of feed ('аренда')
    https://yandex.ru/support/realty/requirements/requirements-sale-housing.html

--- src/Enums/Type.php
+++ src/Enums/Type.php
@@ -8,8 +8,10 @@ use MyCLabs\Enum\Enum;
  * Class Type
  * @package Windstep\YRLGenerator\Enums
  * @method static SELL()
+ * @method static RENT()
  */
 class Type extends Enum
 {
     private const SELL = 'продажа';
+    private const RENT = 'аренда';
 }
